38aabd0e96184d7e39df0307ffecf15f7cc27521
Pavel <ivanovpavelalex45@gmail.com>
2019-06-02 04:13:16 +0500

    Added remove operation of lens

--- src/fun/lens-fp.js
+++ src/fun/lens-fp.js
@@ -5,7 +5,7 @@ const lens = source => ({
   get: instance => instance[source],
   set: (value, instance) => ({ ...instance, [source]: value }),
   delete: instance => {
-    const { [source]: forgot, rest} = instance;
+    const { [source]: forgot, ...rest} = instance;
     return rest;
   }
 });
@@ -13,11 +13,13 @@ const lens = source => ({
 const view = (lens, instance) => lens.get(instance);
 const set = (lens, value, instance) => lens.set(value, instance);
 const map= (lens, f, instance) => lens.set(f(lens.get(instance)), instance);
+const remove = (lens, instance) => lens.delete(instance);
 
 
 module.exports = {
   view,
   set,
   map,
+  remove,
   lens
 };
--- test/test-fun/lens-fp.js
+++ test/test-fun/lens-fp.js
@@ -2,17 +2,17 @@
 
 
 const test = require('ava');
-const { lens, view, set, map } = require('../../src/fun/lens-fp.js');
+const { lens, view, set, map, remove } = require('../../src/fun/lens-fp.js');
 
 
-test('view a property value through lens', t => {
+test('view a property value', t => {
   const arrayLikeLens = lens('length');
   t.is(view(arrayLikeLens, new Array(2)), 2);
   t.is(view(arrayLikeLens, { length: 10 }), 10);
   t.is(view(arrayLikeLens, []), 0);
 });
 
-test('set a property value through lens', t => {
+test('creates iterable entity from source', t => {
   const iterableLens = lens(Symbol.iterator);
   const mock = {};
   const another = set(iterableLens, function* () { yield 101 }, mock);
@@ -21,9 +21,21 @@ test('set a property value through lens', t => {
   }
 });
 
-test('map property value to', t => {
+test('maps success response to not found server answer', t => {
   const httpStatusLens = lens('status');
   const ok = { status: 200 };
   const not_found = map(httpStatusLens, status => 404, ok);
   t.is(not_found.status, 404);
+});
+
+test('removes support of iteration protocol', t => {
+  const iterable = {
+    *[Symbol.iterator]() {
+      yield 101;
+    }
+  };
+  const nonIterableLens = lens(Symbol.iterator);
+  const another = remove(nonIterableLens, iterable);
+  t.true(Symbol.iterator in iterable);
+  t.false(Symbol.iterator in another);
 });
