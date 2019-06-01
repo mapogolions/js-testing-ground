ed5b11a9f83d3d887635af00285627cc492f640b
Pavel <ivanovpavelalex45@gmail.com>
2019-06-01 23:57:45 +0500

    Implemented lens

--- src/fun/lens.js
+++ src/fun/lens.js
@@ -3,9 +3,14 @@
 
 const defaultGetter = prop => instance => instance[prop];
 const defaultSetter = prop => (value, instance) => ({ ...instance, [prop]: value });
+const lens = (getter, setter) => ({
+  get: instance => getter(instance),
+  set: (value, instance) => setter(value, instance)
+});
 
 
 module.exports = {
   defaultGetter,
-  defaultSetter
+  defaultSetter,
+  lens
 };
--- test/test-fun/lens-test.js
+++ test/test-fun/lens-test.js
@@ -2,7 +2,7 @@
 
 
 const test = require('ava');
-const { defaultGetter, defaultSetter } = require('../../src/fun/lens.js');
+const { defaultGetter, defaultSetter, lens } = require('../../src/fun/lens.js');
 
 
 test('returns value of object property', t => {
@@ -17,4 +17,19 @@ test('overwrite existing property of object', t => {
   t.true(origin !== another);
   t.is(origin.name, 'Bob');
   t.is(another.name, 'Sam');
+});
+
+test('get a property value through lens', t => {
+  const httpLens = lens(defaultGetter('status'), defaultSetter('status'));
+  t.is(httpLens.get({ status: 200 }), 200);
+  t.is(httpLens.get({ message: 'Ok' }), undefined);
+});
+
+test('set a property value through lens', t => {
+  const httpLens = lens(defaultGetter('status'), defaultSetter('status'));
+  const response = { status: 200 };
+  const another = httpLens.set(404, response);
+  t.true(response !== another);
+  t.is(response.status, 200);
+  t.is(another.status, 404);
 });
