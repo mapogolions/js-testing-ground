6f9536b9c3c18f073b0d240b03207f5dbce0ff57
Pavel <ivanovpavelalex45@gmail.com>
2019-06-06 13:13:03 +0500

    Implemented pipeline of async functions

--- src/fun/pipeline.js
+++ src/fun/pipeline.js
@@ -1,4 +1,4 @@
-const { compose } = require('./compose.js');
+const { compose, asyncCompose } = require('./compose.js');
 
 
 function foldLeft(f, seed, source) {
@@ -12,5 +12,21 @@ function foldLeft(f, seed, source) {
 const pipeline = (...fs) => foldLeft((seed, f) => compose(f, seed), x => x, fs);
 const backPipeline = (...fs) => foldLeft((seed, f) => compose(seed, f), x => x, fs);
 
+const asyncPipeline = (...fs) => foldLeft(
+  (seed, f) => asyncCompose(f, seed),
+  async x => x,
+  fs,
+);
+const backAsyncPipeline = (...fs) => foldLeft(
+  (seed, f) => asyncCompose(seed, f),
+  async x => x,
+  fs,
+);
 
-module.exports = { pipeline, backPipeline };
+
+module.exports = {
+  pipeline,
+  backPipeline,
+  asyncPipeline,
+  backAsyncPipeline,
+};
--- test/test-fun/pipeline-test.js
+++ test/test-fun/pipeline-test.js
@@ -1,11 +1,19 @@
 const test = require('ava');
-const { pipeline, backPipeline } = require('../../src/fun/pipeline.js');
+const {
+  pipeline, backPipeline, asyncPipeline, backAsyncPipeline,
+} = require('../../src/fun/pipeline.js');
 
 
-test('builds pipeline from sequence of functions', (t) => {
+test('builds pipeline from sequence of sync functions', (t) => {
   const fs = [x => x + 1, x => x ** 2, x => x - 1];
-  const f = pipeline(...fs);
-  const g = backPipeline(...fs);
+  const [f, g] = [pipeline(...fs), backPipeline(...fs)];
   t.is(f(5), 35);
   t.is(g(5), 17);
+});
+
+test('builds pipeline from sequence of async functions', async (t) => {
+  const fs = [async x => x + 1, async x => x ** 2, async x => x - 1];
+  const [f, g] = [asyncPipeline(...fs), backAsyncPipeline(...fs)];
+  t.is(await f(5), 35);
+  t.is(await g(5), 17);
 });
