d0b94b6ed1e0a41c2419fa24f4a389d851a48a87
Pavel <ivanovpavelalex45@gmail.com>
2019-06-08 03:22:37 +0500

    Added tests CPS filter function

--- test/test-fun/array-cps-test.js
+++ test/test-fun/array-cps-test.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const { map } = require('../../src/fun/array-cps.js');
+const { map, filter } = require('../../src/fun/array-cps.js');
 
 
 test.cb('map with empty array', (t) => {
@@ -58,6 +58,48 @@ test.cb('different execution time', (t) => {
     (x, callback) => setTimeout(() => callback(null, x + 1), even(x) ? 12 : 0),
     (err, result) => {
       t.deepEqual(result, expected);
+      t.end();
+    },
+  );
+});
+
+test.cb('filter with empty array', (t) => {
+  filter(
+    [],
+    (x, callback) => process.nextTick(() => callback(null, x > 0)),
+    (err, result) => {
+      t.deepEqual(result, []);
+      t.end();
+    },
+  );
+});
+
+test.cb('ignores odd numbers', (t) => {
+  const even = x => x % 2 === 0;
+  const failure = new Error('Fake error');
+  filter(
+    [1, 2, 3, 4],
+    (x, callback) => {
+      if (even(x)) {
+        process.nextTick(() => callback(null, true));
+      } else {
+        process.nextTick(() => callback(failure));
+      }
+    },
+    (err, result) => {
+      t.deepEqual(result, [2, 4]);
+      t.end();
+    },
+  );
+});
+
+test.cb('ignores even numbers', (t) => {
+  const odd = x => x % 2 !== 0;
+  filter(
+    [1, 2, 3, 4, 5, 6],
+    (x, callback) => process.nextTick(() => callback(null, odd(x))),
+    (err, result) => {
+      t.deepEqual(result, [1, 3, 5]);
       t.end();
     },
   );
