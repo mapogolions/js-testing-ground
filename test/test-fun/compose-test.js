baea6914b6c2d230da9404b7bee2e2e70cae3245
Pavel <ivanovpavelalex45@gmail.com>
2019-06-07 06:19:52 +0500

    Refactoring: removed 'magic' names

--- test/test-fun/compose-test.js
+++ test/test-fun/compose-test.js
@@ -3,44 +3,44 @@ const { compose, composeAsync, composeCps } = require('../../src/fun/compose.js'
 
 
 test('traditional functional composition', (t) => {
-  const f = compose(x => x ** 2, x => x + 1);
-  const g = compose(x => x % 2 === 0, x => x + 1);
-  t.is(f(1), 4);
-  t.is(f(8), 81);
-  t.true(g(1));
-  t.false(g(2));
+  const incrementThenSquare = compose(x => x ** 2, x => x + 1);
+  const isEvenAfterIncrement = compose(x => x % 2 === 0, x => x + 1);
+  t.is(incrementThenSquare(1), 4);
+  t.is(incrementThenSquare(8), 81);
+  t.true(isEvenAfterIncrement(1));
+  t.false(isEvenAfterIncrement(2));
 });
 
 test('composition of asynchronous functions', async (t) => {
-  const f = composeAsync(async x => x ** 2, async x => x + 1);
-  const g = composeAsync(async x => x % 2 === 0, x => x + 1);
-  t.is(await f(1), 4);
-  t.is(await f(8), 81);
-  t.true(await g(1));
-  t.false(await g(2));
+  const incrementThenSquare = composeAsync(async x => x ** 2, async x => x + 1);
+  const isEvenAfterIncrement = composeAsync(async x => x % 2 === 0, x => x + 1);
+  t.is(await incrementThenSquare(1), 4);
+  t.is(await incrementThenSquare(8), 81);
+  t.true(await isEvenAfterIncrement(1));
+  t.false(await isEvenAfterIncrement(2));
 });
 
 test('composition of functions using continuation passing style', (t) => {
-  const f = composeCps(
+  const incrementThenSquare = composeCps(
     (x, callback) => callback(null, x ** 2),
     (x, callback) => callback(null, x + 1),
   );
-  const g = composeCps(
+  const isEvenAfterIncrement = composeCps(
     (x, callback) => callback(null, x % 2 === 0),
     (x, callback) => callback(null, x + 1),
   );
-  f(1, (err, payload) => t.is(payload, 4));
-  f(8, (err, payload) => t.is(payload, 81));
-  g(1, (err, payload) => t.true(payload));
-  g(2, (err, payload) => t.false(payload));
+  incrementThenSquare(1, (err, payload) => t.is(payload, 4));
+  incrementThenSquare(8, (err, payload) => t.is(payload, 81));
+  isEvenAfterIncrement(1, (err, payload) => t.true(payload));
+  isEvenAfterIncrement(2, (err, payload) => t.false(payload));
 });
 
 test.cb('composition of functions using asynchronous continuation passing style', (t) => {
-  const f = composeCps(
+  const incrementThenSquare = composeCps(
     (content, callback) => setTimeout(() => callback(null, content.split('\n')), 10),
     (fileName, callback) => setTimeout(() => callback(null, 'foo\nbar\n'), 15),
   );
-  f('fake.md', (err, lines) => {
+  incrementThenSquare('fake.md', (err, lines) => {
     t.deepEqual(lines, ['foo', 'bar', '']);
     t.end();
   });
--- test/test-fun/pipeline-test.js
+++ test/test-fun/pipeline-test.js
@@ -11,16 +11,16 @@ const {
 
 test('builds pipeline from sequence of sync functions', (t) => {
   const fs = [x => x + 1, x => x ** 2, x => x - 1];
-  const [f, g] = [pipeline(...fs), backPipeline(...fs)];
-  t.is(f(5), 35);
-  t.is(g(5), 17);
+  const [forwardFlow, backwardFlow] = [pipeline(...fs), backPipeline(...fs)];
+  t.is(forwardFlow(5), 35);
+  t.is(backwardFlow(5), 17);
 });
 
 test('builds pipeline from sequence of async functions', async (t) => {
   const fs = [async x => x + 1, async x => x ** 2, async x => x - 1];
-  const [f, g] = [pipelineAsync(...fs), backPipelineAsync(...fs)];
-  t.is(await f(5), 35);
-  t.is(await g(5), 17);
+  const [forwardFlow, backwardFlow] = [pipelineAsync(...fs), backPipelineAsync(...fs)];
+  t.is(await forwardFlow(5), 35);
+  t.is(await backwardFlow(5), 17);
 });
 
 test('builds pipeline from sequence of functions using CPS', (t) => {
@@ -29,9 +29,9 @@ test('builds pipeline from sequence of functions using CPS', (t) => {
     (x, callback) => callback(null, x ** 2),
     (x, callback) => callback(null, x - 1),
   ];
-  const [f, g] = [pipelineCps(...fs), backPipelineCps(...fs)];
-  t.is(f(5, (err, payload) => t.is(payload, 35)));
-  t.is(g(5, (err, payload) => t.is(payload, 17)));
+  const [forwardFlow, backwardFlow] = [pipelineCps(...fs), backPipelineCps(...fs)];
+  t.is(forwardFlow(5, (err, payload) => t.is(payload, 35)));
+  t.is(backwardFlow(5, (err, payload) => t.is(payload, 17)));
 });
 
 test.cb('builds pipeline from sequence of functions using async CPS', (t) => {
@@ -40,7 +40,8 @@ test.cb('builds pipeline from sequence of functions using async CPS', (t) => {
     (x, callback) => setTimeout(() => callback(null, x ** 2), 3),
     (x, callback) => setTimeout(() => callback(null, x - 1), 10),
   ];
-  pipelineCps(...fs)(5, (err, payload) => {
+  const forwardFlow = pipelineCps(...fs);
+  forwardFlow(5, (err, payload) => {
     t.is(payload, 35);
     t.end();
   });
