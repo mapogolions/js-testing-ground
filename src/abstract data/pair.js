80c7e25a5494dd8843b634d79a318ad1f9b68d33
Pavel <ivanovpavelalex45@gmail.com>
2019-06-13 19:53:10 +0500

    Implemented: immutable tuple and pair

---
+++ src/abstract data/pair.js
@@ -0,0 +1,20 @@
+/* eslint-disable space-infix-ops */
+const requiredArgument = () => {
+  throw new TypeError('Missing required positional argument');
+};
+
+const Pair = (first=requiredArgument(), second=requiredArgument()) => (index) => {
+  if (index === 1) return first;
+  if (index === 2) return second;
+  throw new RangeError('Pair index out of range');
+};
+
+const Tuple = (...items) => (index) => {
+  if (index < 0 || index >= items.length) {
+    throw new RangeError('Tuple index out of range');
+  }
+  return items[index];
+};
+
+
+module.exports = { Pair, Tuple };
