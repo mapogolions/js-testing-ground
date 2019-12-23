67eacdd25d4e459c41c8e74a9b8cc1316d518327
mapogolions <ivanovpavelalex45@gmail.com>
2019-12-23 22:38:49 +0500

    implement follows function

--- src/fun/matching.js
+++ src/fun/matching.js
@@ -2,5 +2,16 @@
 
 const just = target => source => source.startsWith(target) && target;
 
+const follows = (...patterns) => source => {
+  let matchUpTo = 0;
+  let rest = source;
+  for (const pattern of patterns) {
+    const matched = pattern(rest);
+    if (matched === false) return false;
+    matchUpTo += matched.length;
+    rest = rest.slice(matched.length);
+  }
+  return source.slice(0, matchUpTo);
+};
 
-module.exports = { just };
+module.exports = { just, follows };
--- test/test-fun/matching-test.js
+++ test/test-fun/matching-test.js
@@ -1,6 +1,17 @@
 const test = require('ava');
-const { just } = require ('../../src/fun/matching.js');
+const { just, follows } = require ('../../src/fun/matching.js');
 
+
+test('follows', t => {
+  const match = follows(
+    just('()'),
+    just('<'),
+    just('>')
+  );
+  t.is(match('()<>'), '()<>');
+  t.false(match('()<'));
+  t.false(match(''));
+});
 
 test('just', t => {
   t.is(just('(')('()'), '(');
