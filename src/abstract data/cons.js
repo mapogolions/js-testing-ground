e520ee43aa71cf0a207ac50905b47717c639f7ce
mapogolions <ivanovpavelalex45@gmail.com>
2019-12-25 21:33:50 +0500

    add CELL_SYMBOL

--- src/abstract data/cons.js
+++ src/abstract data/cons.js
@@ -1,19 +1,21 @@
 'use strict';
 
-/* const cons = (fst, snd) => f => f(fst, snd);
-const car = pair => pair((x, y) => x);
-const cdr = pair => pair((x, y) => y);
-const empty = xs => Object.is(xs, nil);
-const isPair = xs => typeof xs === 'function';
+/**
+ * const cons = (fst, snd) => f => f(fst, snd);
+ * const car = pair => pair((x, y) => x);
+ * const cdr = pair => pair((x, y) => y);
+ * const empty = xs => Object.is(xs, nil);
+ * const isPair = xs => typeof xs === 'function';
  */
-const nil = Object.freeze({ ['@@__cons__@@']: false });
-const cons = (head, tail) => Object.freeze({ head, tail, ['@@__cons__@@']: true });
+const CELL_SYMBOL = '@@__cons__@@';
+const nil = Object.freeze({ [CELL_SYMBOL]: false });
+const cons = (head, tail) => Object.freeze({ head, tail, [CELL_SYMBOL]: true });
 const car = ({ head }) => head;
 const cdr = ({ tail }) => tail;
 const cadr = ({ tail }) => car(tail);
 const caddr = ({ tail }) => car(cdr(tail));
 const empty = xs => Object.is(xs, nil);
-const isPair = xs => xs['@@__cons__@@'];
+const isPair = xs => xs[CELL_SYMBOL];
 
 const list = (...values) => (values.length <= 0
   ? nil : cons(values[0], list(...values.slice(1))));
