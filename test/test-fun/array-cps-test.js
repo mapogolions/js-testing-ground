33b9e77099627da14f52db80b0dd240d3107db59
mapogolions <ivanovpavelalex45@gmail.com>
2020-01-26 19:27:05 +0500

    Update tests for

--- test/test-fun/array-cps-test.js
+++ test/test-fun/array-cps-test.js
@@ -139,22 +139,22 @@ test.cb('iteration over an empty array', t => {
   );
 });
 
-test.cb('error occurrence stops iteration', t => {
+test.cb('error should break iteration', t => {
   const sideEffect = [];
   const failure = new Error('Negative number');
   each(
     [1, -2, 2, -3],
     (item, callback) => process.nextTick(() => {
-      sideEffect.push(item);
       if (item < 0) {
         callback(failure);
         return;
       }
+      sideEffect.push(item);
       callback(null);
     }),
     (err) => {
       t.is(err, failure);
-      t.deepEqual(sideEffect, [1, -2]);
+      t.deepEqual(sideEffect, [1]);
       t.end();
     },
   );
@@ -171,6 +171,24 @@ test.cb('successful iteration over an array', t => {
     (err) => {
       t.is(err, null);
       t.deepEqual(sideEffect, [1, 2, 3]);
+      t.end();
+    },
+  );
+});
+
+test.cb('each should have parallel semantics', t => {
+  const sideEffect = [];
+  each(
+    [1, 2, 3, 4],
+    (item, callback) => {
+      const delay = 100 - (item * 10);
+      setTimeout(() => {
+        sideEffect.push(item);
+        callback(null);
+      }, delay);
+    },
+    (_err, _result) => {
+      t.deepEqual(sideEffect, [4, 3, 2, 1]);
       t.end();
     },
   );
