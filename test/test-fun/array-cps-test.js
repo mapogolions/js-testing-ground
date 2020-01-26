25794c6d1040870d3f367b97964c4d3abb605866
mapogolions <ivanovpavelalex45@gmail.com>
2020-01-26 20:49:36 +0500

    Update tests for

--- test/test-fun/array-cps-test.js
+++ test/test-fun/array-cps-test.js
@@ -299,7 +299,8 @@ test.cb('empty array does not pass the test implemented by the provided function
   );
 });
 
-test.cb('`some` should return at least one ', t => {
+test.cb('`some` should ignore an error', t => {
+  const failure = new Error('Some error');
   some(
     [1, 2, -1, 0],
     (item, callback) => process.nextTick(() => {
@@ -307,7 +308,7 @@ test.cb('`some` should return at least one ', t => {
         callback(null, true);
         return;
       }
-      callback(null, false);
+      callback(failure);
     }),
     (err, result) => {
       t.is(err, null);
@@ -317,19 +318,49 @@ test.cb('`some` should return at least one ', t => {
   );
 });
 
-test.cb('`some` should ignore errors', t => {
+test.cb('`some` should return true when at least one element is a negative number', t => {
   some(
     [1, 2, -1, 0],
-    (item, callback) => process.nextTick(() => {
-      if (item < 0) {
-        callback(null, true);
-        return;
-      }
-      callback(new Error('Positive number'));
-    }),
+    (item, callback) => process.nextTick(() => callback(null, item < 0)),
     (err, result) => {
       t.is(err, null);
       t.true(result);
+      t.end();
+    },
+  );
+});
+
+test.cb('`some`should have parallel semantics', t => {
+  const sideEffect = [];
+  some(
+    [1, 2, 3, 4],
+    (item, callback) => {
+      const delay = 100 - (item * 10);
+      setTimeout(() => {
+        sideEffect.push(item);
+        callback(null, false);
+      }, delay);
+    },
+    (_err, _result) => {
+      t.deepEqual(sideEffect, [4, 3, 2, 1]);
+      t.end();
+    },
+  );
+});
+
+test.cb('`some`should be lazy', t => {
+  const sideEffect = [];
+  some(
+    [1, 2, 3, 4],
+    (item, callback) => {
+      const delay = 100 - (item * 10);
+      setTimeout(() => {
+        sideEffect.push(item);
+        callback(null, item % 2 === 0);
+      }, delay);
+    },
+    (_err, _result) => {
+      t.deepEqual(sideEffect, [4]);
       t.end();
     },
   );
